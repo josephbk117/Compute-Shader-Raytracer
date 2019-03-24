216bb5506f4b97b38c75e08c7c3be6b14c440645
joseph kalathil <josephbk.117@hotmail.com>
2019-03-24 16:53:59 +0530

    Initial commit

---
+++ Assets/Scripts/RayTracingMaster.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour
+{
+    public ComputeShader RayTracingShader;
+    public Texture SkyboxTexture;
+
+    private RenderTexture _target;
+    private Camera _camera;
+    private uint _currentSample = 0;
+    private Material _addMaterial;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (transform.hasChanged)
+        {
+            _currentSample = 0;
+            transform.hasChanged = false;
+        }
+    }
+
+    private void SetShaderParameters()
+    {
+        RayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        RayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        RayTracingShader.SetTexture(0, "_SkyboxTexture", SkyboxTexture);
+        RayTracingShader.SetVector("_PixelOffset", new Vector2(Random.value, Random.value));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Render(destination);
+    }
+    private void Render(RenderTexture destination)
+    {
+        // Make sure we have a current render target
+        InitRenderTexture();
+        // Set the target and dispatch the compute shader
+        RayTracingShader.SetTexture(0, "Result", _target);
+        SetShaderParameters();
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        // Blit the result texture to the screen
+        if (_addMaterial == null)
+            _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
+        _addMaterial.SetFloat("_Sample", _currentSample);
+        Graphics.Blit(_target, destination, _addMaterial);
+        _currentSample++;
+    }
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null)
+                _target.Release();
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+}
