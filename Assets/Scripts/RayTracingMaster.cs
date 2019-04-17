663ac6a368a5a4a01a9c99c5e346f5573e60e0f2
joseph kalathil <josephbk.117@hotmail.com>
2019-04-17 21:56:00 +0530

    lambert diffuse with monte carlo integration added

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -17,9 +17,11 @@ public class RayTracingMaster : MonoBehaviour
     public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
     public uint SpheresMax = 100;
     public float SpherePlacementRadius = 100.0f;
+    public int SphereSeed;
 
     private ComputeBuffer _sphereBuffer;
     private RenderTexture _target;
+    private RenderTexture _converged;
     private Camera _camera;
     private uint _currentSample = 0;
     private Material _addMaterial;
@@ -43,6 +45,7 @@ public class RayTracingMaster : MonoBehaviour
 
     private void SetUpScene()
     {
+        Random.InitState(SphereSeed);
         List<Sphere> spheres = new List<Sphere>();
         // Add a number of random spheres
         for (int i = 0; i < SpheresMax; i++)
@@ -61,7 +64,7 @@ public class RayTracingMaster : MonoBehaviour
             }
             // Albedo and specular color
             Color color = Random.ColorHSV();
-            bool metal = Random.value < 0.5f;
+            bool metal = Random.value < 0.0f;
             sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
             sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
             // Add the sphere to the list
@@ -92,6 +95,7 @@ public class RayTracingMaster : MonoBehaviour
         Vector3 dirLight = DirectionalLight.transform.forward;
         RayTracingShader.SetVector("_DirectionalLight", new Vector4(dirLight.x, dirLight.y, dirLight.z, DirectionalLight.intensity));
         RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+        RayTracingShader.SetFloat("_Seed", Random.value);
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -112,7 +116,8 @@ public class RayTracingMaster : MonoBehaviour
         if (_addMaterial == null)
             _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
         _addMaterial.SetFloat("_Sample", _currentSample);
-        Graphics.Blit(_target, destination, _addMaterial);
+        Graphics.Blit(_target, _converged, _addMaterial);
+        Graphics.Blit(_converged, destination);
         _currentSample++;
     }
     private void InitRenderTexture()
@@ -127,6 +132,18 @@ public class RayTracingMaster : MonoBehaviour
                 RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
             _target.enableRandomWrite = true;
             _target.Create();
+        }
+
+        if (_converged == null || _converged.width != Screen.width || _converged.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_converged != null)
+                _converged.Release();
+            // Get a render target for Ray Tracing
+            _converged = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _converged.enableRandomWrite = true;
+            _converged.Create();
         }
     }
 }
